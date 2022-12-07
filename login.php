73db0f7c3a539dc3173426eb7c86238a219874e2
Maiyoj <valentinemaiyo1995@gmail.com>
2022-12-07 13:05:15 +0300

    login functional

--- login.php
+++ login.php
@@ -1,23 +1,20 @@
 <?php include 'config.php'; ?>
 <?php
     session_start();
-    include('config.php');
     if (isset($_POST['login'])) {
         $username = $_POST['username'];
         $password = $_POST['password'];
-        $query = $connection->prepare("SELECT * FROM users WHERE id=:id");
-        $query->bindParam("username", $username, PDO::PARAM_STR);
-        $query->execute();
-        $result = $query->fetch(PDO::FETCH_ASSOC);
-        if (!$result) {
-            echo '<p class="error">Username password combination is wrong!</p>';
-        } else {
-            if (password_verify($password, $result['password'])) {
-                $_SESSION['user_id'] = $result['id'];
-                echo '<p class="success">Congratulations, you are logged in!</p>';
-            } else {
-                echo '<p class="error">Username password combination is wrong!</p>';
-            }
-        }
+
+        $sql_u = "SELECT * FROM users WHERE username='$username'";
+        $sql_e = "SELECT * FROM users WHERE password='$password'";
+        $res_u = mysqli_query($connection, $sql_u);
+        $res_e = mysqli_query($connection, $sql_e);
+  
+        if (mysqli_num_rows($res_u) > 0) {
+          //$results = mysqli_query($connection, $query);
+          echo 'Loginn!';
+          exit();
     }
+}
+    
 ?>
